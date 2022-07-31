78837d4b9dec76a0a5e76035ba3c96a7887d98e2
Connectgauravkaushik <101702222+Connectgauravkaushik@users.noreply.github.com>
2022-07-31 22:41:55 +0530

    Add files via upload

---
+++ Data Structure (problem.1)/src/com/greatlearning/ConstructBuilding/ConstructFloor.java
@@ -0,0 +1,35 @@
+package com.greatlearning.ConstructBuilding;
+import java.util.*;
+public class ConstructFloor {
+	
+	 public static void main(String[]args) {
+		 
+		   Scanner sc = new Scanner(System.in);
+		   System.out.println("Enter the total number floors in the building");
+		   int floorsize = sc.nextInt();
+		   
+		   int arr[]=new int[floorsize+1];
+		   for(int i=1; i<floorsize+1; i++) {
+			   System.out.println("Enter the floor size given on day" +i);
+			   int m = sc.nextInt();
+			   arr[m]=i;
+		  } 
+		   
+		   int j=floorsize;
+		   boolean flag;
+		   System.out.println("The order of Construction is as Follows:");
+		   
+		   for(int i=1; i<=floorsize;i++) {
+			   flag=false;
+			   System.out.println("Day : " +i);
+			   while(j>=1 && arr[j]<=i) {
+				   flag=true;
+				   System.out.print(j+" ");
+				   j--;
+			   }
+			   if(flag==true) {
+				   System.out.println();
+			   }
+		   }
+	   }
+	}
